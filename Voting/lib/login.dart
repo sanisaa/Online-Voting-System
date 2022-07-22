3b64aa566730d8b6b38dad1347b0d00fe732e79b
Sanisha <sanisamhrzn@gmail.com>
2022-07-22 18:44:43 +0545

    changes made

--- Voting/lib/login.dart
+++ Voting/lib/login.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-//Login page
+//Login 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
