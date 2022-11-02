ea68de46db3678e9aa7bd3be020a4615d0bf1a3f
Sanisha <sanisamhrzn@gmail.com>
2022-11-02 23:22:10 +0545

    total voterss php

--- php/totalvoters_count.php
+++ php/totalvoters_count.php
@@ -1,15 +1,7 @@
-<<<<<<<< HEAD:php/totalvoters_count.php
+
 <?php 
 $connection = new mysqli("localhost","root","","election");
-//$vote=0;
-========
-<?php
-    //voter=2
-    //candidate=3
-    //admin=1
-    $connection = new mysqli("localhost","root","","election");
-   
->>>>>>>> dbc59055cbbb646a0475b0b477e8b00a8603151d:php/totalvoters.php
+
 
     $sql = "select * from user where rid!=1";
 
